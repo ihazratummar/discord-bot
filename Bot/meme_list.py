97324399a5edb19d45f5cd7996658ddaf0955fb8
ihazratummar <hazratummar9@gmail.com>
2023-05-06 21:51:58 +0530

    restore

--- Bot/meme_list.py
+++ Bot/meme_list.py
@@ -1,4 +1,8 @@
 import random
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import discord
 
 RANDOM_MEME_LIST = [
     "https://cdn.discordapp.com/attachments/874162166343815229/1103965676512755792/meme.png",
@@ -73,9 +77,26 @@ RANDOM_MEME_LIST = [
     "https://cdn.discordapp.com/attachments/1104003925369176124/1104006099578912819/which_side_are_you_on_f85d5dd3e3b8951c0d881520a1f7ccf3.jpeg",
     "https://cdn.discordapp.com/attachments/1104003925369176124/1104006108303077527/Why_is_she_like_this_2154dacd38db929f024768ed8dad86e9.jpeg",
     "https://cdn.discordapp.com/attachments/1104003925369176124/1104006129681432687/woof_woof_I_love_you_bestie._91205b6e313e7baa0436f443c85b1740.jpeg",
-    "https://cdn.discordapp.com/attachments/1104003925369176124/1104006139328340008/Yes_it_is_coming_in_some_countries_on_the_App_Store_it_has_already_come_out_f00a13583537782ab3e1353b49cee822.png"
+    "https://cdn.discordapp.com/attachments/1104003925369176124/1104006139328340008/Yes_it_is_coming_in_some_countries_on_the_App_Store_it_has_already_come_out_f00a13583537782ab3e1353b49cee822.png",
 ]
 
 meme_url = random.choice(RANDOM_MEME_LIST)
 
 print(meme_url)
+
+# for i in RANDOM_MEME_LIST:
+#     responses = requests.get(meme_url)
+#     image = Image.open(BytesIO(responses.content))
+
+#     caption = "Meme"
+#     draw = ImageDraw.Draw(image)
+#     font = ImageFont.truetype("Lato-Bold.ttf", 20)
+#     draw.text((10, 10), caption, font=font, fill="white")
+
+#     with BytesIO() as image_binary:
+#         image.save(image_binary, "PNG")
+#         image_binary.seek(0)
+#         file = discord.File(fp=image_binary, filename="meme.png")
+
+#     if __name__ == "__main__":
+#         RANDOM_MEME_LIST = RANDOM_MEME_LIST
--- Bot/slash_commands.py
+++ Bot/slash_commands.py
@@ -1,7 +1,7 @@
-#region imports <- This is foldable
+# region imports <- This is foldable
 from config import *
 from http.client import *
-from urllib import *
+from urllib import request
 import requests
 import random
 import discord
@@ -13,111 +13,152 @@ from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 from meme_list import meme_url
 import json
-#endregion
+import wget
+import os
+
+# endregion
+
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    print("Bot is ready.")
     try:
         synced = await client.tree.sync()
         print(f"Synced {len(synced)} commands(s)")
     except Exception as e:
-        print(e)   
-        
-@client.tree.command(name="ping", description = "Check your ping")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Pong! {round(client.latency * 1000)}ms')   
+        print(e)
 
-@client.tree.command(name="quota", description = "Display quota")
+
+@client.tree.command(name="ping", description="Check your ping")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Pong! {round(client.latency * 1000)}ms")
+
+
+@client.tree.command(name="quota", description="Display quota")
 async def quota(interaction: discord.Interaction):
     responses = requests.get("https://api.quotable.io/random")
     data = responses.json()
     quota = data["content"]
     author = data["author"]
-    await interaction.response.send_message(f"{author}:\n\n━━━━{quota}")    
-    
-@client.tree.command(name="wiki", description = "Search wikipedia")
+    await interaction.response.send_message(f"{author}:\n\n━━━━{quota}")
+
+
+@client.tree.command(name="wiki", description="Search wikipedia")
 async def wiki(interaction: discord.Interaction, query: str):
     import wikipedia
+
     wikipedia.set_lang("en")
     result = wikipedia.summary(query, sentences=2)
-    await interaction.response.send_message(result)           
+    await interaction.response.send_message(result)
+
 
 @client.tree.command(name="pc", description="Check my Gaming and Streaming pc Config")
 async def pc(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Gaming PC\n\nCPU:- Ryzen 7 3700x.\nGraphics Card:- 2060 super 8gb.\nMemory - XPG 16GB 3600MHz\nStorage - 1 TB SSD /1TB HDD\n\nStreaming PC\n\nCPU: Intel i3 7th gen\nGPU: 1050Ti 4GB OC\nRAM: 8 GB 2400 MHz\nStorage: 250 GB SSD\nElgato HD60s 14000\nTotal Price:-124500") 
+    await interaction.response.send_message(
+        f"Gaming PC\n\nCPU:- Ryzen 7 3700x.\nGraphics Card:- 2060 super 8gb.\nMemory - XPG 16GB 3600MHz\nStorage - 1 TB SSD /1TB HDD\n\nStreaming PC\n\nCPU: Intel i3 7th gen\nGPU: 1050Ti 4GB OC\nRAM: 8 GB 2400 MHz\nStorage: 250 GB SSD\nElgato HD60s 14000\nTotal Price:-124500"
+    )
 
-@client.tree.command(name="whatsapp", description="whatsapp  group daily stream notification")
+
+@client.tree.command(
+    name="whatsapp", description="whatsapp  group daily stream notification"
+)
 async def whatsapp(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Join Whatsapp Group for stream notification : https://bit.ly/3zBxoCf')
+    await interaction.response.send_message(
+        f"Join Whatsapp Group for stream notification : https://bit.ly/3zBxoCf"
+    )
+
 
 @client.tree.command(name="support")
 async def support(interaction: discord.Interaction):
-    await interaction.response.send_message(f'You can support us by becoming a facebook page member.\nBecome a supporter:-  https://bit.ly/3xJdNzZ') 
+    await interaction.response.send_message(
+        f"You can support us by becoming a facebook page member.\nBecome a supporter:-  https://bit.ly/3xJdNzZ"
+    )
+
 
 @client.tree.command(name="social")
 async def social(interaction: discord.Interaction):
-    await interaction.response.send_message(f'You can check all my social profile here :\nhttps://wlo.link/@crazyforsurprise')    
+    await interaction.response.send_message(
+        f"You can check all my social profile here :\nhttps://wlo.link/@crazyforsurprise"
+    )
+
 
 @client.tree.command(name="meme", description="Get random meme")
 async def get_meme(interaction: discord.Interaction):
+    # opener = urllib.request.build_opener()
+    # opener.addheaders = [("User-Agent", "Mozilla/5.0")]
+    # urllib.request.install_opener(opener)
+    # for i in RANDOM_MEME_LIST:
+    # try:
+    #     filename = wget.download(i)
+    #     print(f"Image downloaded to {filename}")
+    # except urllib.error.HTTPError as e:
+    #     print(f"Failed to download image. Error code: {e.code}")
     # meme_url = random.choice(meme_list)
     responses = requests.get(meme_url)
-    image =Image.open(BytesIO(responses.content))
-    
+    image = Image.open(BytesIO(responses.content))
+
     caption = "Meme"
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("Lato-Bold.ttf", 20)
-    draw.text((10, 10), caption, font=font, fill ="white")
-    
+    draw.text((10, 10), caption, font=font, fill="white")
+
     with BytesIO() as image_binary:
-        image.save(image_binary, 'PNG')
+        image.save(image_binary, "PNG")
         image_binary.seek(0)
-        file = discord.File(fp=image_binary, filename='meme.png')
-        
-        # Send the meme to the Discord channel
-        await interaction.response.send_message(file=file)
- 
+        file = discord.File(fp=image_binary, filename="meme.png")
+
+    # Send the meme to the Discord channel
+    await interaction.response.send_message(file=file)
+
+
 @client.tree.command(name="youtube", description="search video")
 async def youtube(interaction: discord.Interaction, search: str):
     response = requests.get(f"https://youtube.com/results?search_query={search}")
-    html = response.text 
+    html = response.text
     index = html.find("/watch?v=")
-    url = "https://www.youtube.com" + html[index:index+20]
+    url = "https://www.youtube.com" + html[index : index + 20]
     await interaction.response.send_message(url)
-    
-@client.tree.command(name="warn", description="Warn a member") 
-async def warn(interaction: discord.Interaction, user: discord.Member, * , reason: str):
+
+
+@client.tree.command(name="warn", description="Warn a member")
+async def warn(interaction: discord.Interaction, user: discord.Member, *, reason: str):
     embed = discord.Embed(
-        title= 'User Warning',
-        description= f'{user.mention} has been warned by {interaction.user.mention}',
-        color = discord.Color.dark_purple()
+        title="User Warning",
+        description=f"{user.mention} has been warned by {interaction.user.mention}",
+        color=discord.Color.dark_purple(),
     )
-    
-    embed.add_field(name = 'Reason', value = reason)
-    
-    await interaction.response.send_message(embed=embed)      
-    
+
+    embed.add_field(name="Reason", value=reason)
+
+    await interaction.response.send_message(embed=embed)
+
+
 @client.tree.command(name="imgur", description="search for images")
 async def imgur(interaction: discord.Interaction, *, query: str):
-    headers = {'Authorization': 'Client-ID 20c2904655c6a1f'}
-    params = {'q': query}
-    response = requests.get('https://api.imgur.com/3/gallery/search/', headers=headers, params=params)
+    headers = {"Authorization": "Client-ID 20c2904655c6a1f"}
+    params = {"q": query}
+    response = requests.get(
+        "https://api.imgur.com/3/gallery/search/", headers=headers, params=params
+    )
 
     if response.status_code == 200:
-        data = json.loads(response.content.decode('utf-8'))
-        images = [item for item in data['data'] if 'images' in item and item['images']]
+        data = json.loads(response.content.decode("utf-8"))
+        images = [item for item in data["data"] if "images" in item and item["images"]]
 
         if images:
             random_image = random.choice(images)
-            image_url = random.choice(random_image['images'])['link']
+            image_url = random.choice(random_image["images"])["link"]
 
-            embed = discord.Embed(title=f'Results for "{query}"', color=discord.Color.blue())
+            embed = discord.Embed(
+                title=f'Results for "{query}"', color=discord.Color.blue()
+            )
             embed.set_image(url=image_url)
             await interaction.response.send_message(embed=embed)
         else:
-            await interaction.response.send_message(f'Sorry, no images found for "{query}"')
+            await interaction.response.send_message(
+                f'Sorry, no images found for "{query}"'
+            )
     else:
-        await interaction.response.send_message('Sorry, there was an error processing your request. Please try again later.')      
-        
-        
+        await interaction.response.send_message(
+            "Sorry, there was an error processing your request. Please try again later."
+        )
