a53498609b990dbe66b5dd436c2ab06af4414f7b
ihazratummar <hazratummar9@gmail.com>
2023-05-17 20:46:48 +0530

    r

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,5 @@
 # region imports <- This is foldable
 import discord
-from discord.ext import commands
-from discord import app_commands
 from dotenv import load_dotenv
 from client import bot
 import os
