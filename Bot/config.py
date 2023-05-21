2ad88bc51660177d98c20dfa558136932c37595b
ihazratummar <hazratummar9@gmail.com>
2023-05-21 23:41:54 +0530

    d

--- Bot/config.py
+++ Bot/config.py
@@ -1,48 +1,20 @@
 # region imports <- This is foldable
 import discord
-<<<<<<< HEAD
-=======
 from discord.ext import commands
->>>>>>> temp_branch
 from dotenv import load_dotenv
 from client import bot
 import os
-<<<<<<< HEAD
-import slash_commands
-import mod_commands
-import welcome
-
-
-# endregion
-=======
 from asyncpg.pool import create_pool
 import asyncpg
 
 
 # endregion
 
->>>>>>> temp_branch
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 # db = os.getenv("DB_CONFIG")
 
-<<<<<<< HEAD
-
-@bot.event
-async def on_ready():
-    print("Bot is ready.")
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} commands(s)")
-    except Exception as e:
-        print(e)
-
-
-# this is a test command
-
-bot.run(token)
-=======
 data = os.getenv("database")
 user = os.getenv("username")
 passw = os.getenv("password")
@@ -88,4 +60,3 @@ class Bot(commands.Bot):
 if __name__ == "__main__":
     bot = Bot(command_prefix=".", intents=discord.Intents.all())
     bot.run(token)
->>>>>>> temp_branch
